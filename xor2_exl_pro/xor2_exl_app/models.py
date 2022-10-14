dd510d739e9ead7f9cf3825e64fe561e111cbbf4
d ravi <108458982+ravi944@users.noreply.github.com>
2022-10-14 18:08:57 +0530

    Update models.py

--- xor2_exl_pro/xor2_exl_app/models.py
+++ xor2_exl_pro/xor2_exl_app/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class Person(models.Model):
-    id = models.IntegerField(primary_key=True)
-    first_name    = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    gender = models.CharField(max_length=50)
-    ip_address = models.CharField(max_length=20)
